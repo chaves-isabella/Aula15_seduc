0d83a402fbbc2ed51ab481b99e3351f4a1457fba
Isabella Chaves <isaachaves9@gmail.com>
2026-05-13 22:13:51 -0300

    Update exerc3.java

--- exerc3.java
+++ exerc3.java
@@ -1,4 +1,18 @@
 import static java.lang.IO.*;
 void main() {
-   
+    float totalcompra = 0;
+
+    println("Digite o valor do produto ou 0 para finalizar: ");
+    float precoproduto = Float.parseFloat(readln());
+
+    while(precoproduto != 0) {
+        totalcompra += precoproduto;
+
+        println("Produto adicionado!");
+        println("Digite o valor do próximo produto ou 0 para finalizar: ");
+
+        precoproduto = Float.parseFloat(readln());
+    }
+
+    println("Valor total da compra: R$ " + totalcompra);
 }
